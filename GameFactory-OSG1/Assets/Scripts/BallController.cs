a15f25b7da8825558c9a83bca08914c229a0adbe
MelihCelik00 <melihsafa.c@gmail.com>
2020-06-16 01:29:25 +0300

    new project

---
+++ GameFactory-OSG1/Assets/Scripts/BallController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+   [SerializeField]
+   private float _moveSpeed = 1f;
+   private float _jumpSpeed = 10f;
+
+   private bool isGrounded;
+   private Rigidbody _rigidbody;
+
+   private void Start()
+   {
+      _rigidbody = GetComponent<Rigidbody>();
+   }
+
+   private void Update()
+   {
+      CheckInput();
+   }
+
+   private void CheckInput()
+   {
+      if (Input.GetKey(KeyCode.D))
+      {
+         Move(Vector3.right);
+      }
+      else if (Input.GetKey(KeyCode.A))
+      {
+         Move(Vector3.left);
+      }
+
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+         Jump();
+      }
+   }
+
+   private void Jump()
+   {
+      if (!isGrounded)
+         return;
+      _rigidbody.AddForce(Vector3.up * _jumpSpeed, ForceMode.Impulse);
+   }
+
+   private void Move(Vector3 direction)
+   {
+      _rigidbody.AddForce(direction*_moveSpeed, ForceMode.Acceleration);
+   }
+
+   private void OnCollisionEnter(Collision other)
+   {
+      isGrounded = true;
+   }
+
+   private void OnCollisionExit(Collision other)
+   {
+      isGrounded = false;
+   }
+}
