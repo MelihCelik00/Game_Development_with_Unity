50e68a4c788f7f660829d41ebda243c2811895bb
Melih Safa Çelik <45492251+MelihCelik00@users.noreply.github.com>
2020-06-17 00:26:13 +0300

    enemy and camera follower scripts added

---
+++ GameFactory-OSG1/Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+
+    [SerializeField] 
+    private float _smoothness;
+
+    [SerializeField]
+    private Vector3 _offset;
+    
+    private void FixedUpdate()
+    {
+        if (_target == null)
+        {
+            return;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, _target.position+_offset, Time.deltaTime*_smoothness);
+    }
+}
