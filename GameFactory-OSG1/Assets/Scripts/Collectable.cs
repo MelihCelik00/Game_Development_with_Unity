652fae14ccd57637a69960bcb5d837ca215699ad
Melih Safa Çelik <45492251+MelihCelik00@users.noreply.github.com>
2020-06-16 10:53:33 +0300

    Collectable objects script

---
+++ GameFactory-OSG1/Assets/Scripts/Collectable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+
+    public void Collect()
+    {
+        Destroy(gameObject);
+    }
+    
+}
