d9d8713fc28c30f5ce0182f515a3b644f8f97a55
Melih Safa Çelik <45492251+MelihCelik00@users.noreply.github.com>
2020-06-18 10:35:12 +0300

    Level system and level transition added

---
+++ GameFactory-OSG1/Assets/Scripts/LevelEndZone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEndZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<BallController>()!=null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    
+}
